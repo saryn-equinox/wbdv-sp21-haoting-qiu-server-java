42c9091fef7b055ba5dce5612bb18ea45b4c466a
Haoting Qiu <easonyau@126.com>
2021-01-23 19:49:05 -0500

    first commit

---
+++ src/test/java/com/example/wbdvsp21haotingqiuserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp21haotingqiuserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
